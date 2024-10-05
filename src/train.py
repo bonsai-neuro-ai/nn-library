3fe5a3353bab33f014164bc1dbe67b7709ef843b
wrongu <richard.d.lange.13@dartmouth.edu>
2024-10-05 08:39:22 -0400

    tune_lr as a trainer option

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ from nn_lib.env import add_parser as add_env_parser
 from nn_lib.trainer import add_parser as add_trainer_parser
 from nn_lib.utils import search_runs_by_params
 from lightning.pytorch.loggers import MLFlowLogger
+from lightning.pytorch.tuner import Tuner
 import jsonargparse
 
 
@@ -28,6 +29,19 @@ def main(args: jsonargparse.Namespace, artifacts: dict[str, str] = None):
     # args.data will be a Namespace but args_with_instances.data will be an instance of a
     # LightningDataModule class.
     instantiated_args = parser.instantiate_classes(args)
+
+    # The tune_lr argument is not part of the Trainer class. Pop it.
+    if args.trainer.pop("tune_lr", False):
+        # LR tuning is currently not supported with multi-GPU (DDP) training. We need to first
+        # create a temporary single-GPU trainer to do the tuning.
+        tmp_trainer_args = args.trainer.__dict__.copy()
+        tmp_trainer_args["devices"] = 1
+        tmp_trainer = lit.Trainer(logger=logger, **tmp_trainer_args)
+        tuner = Tuner(tmp_trainer)
+        tuner.lr_find(
+            model=instantiated_args.model, datamodule=instantiated_args.data, update_attr=True
+        )
+        logger.log_hyperparams({"tuned_lr": instantiated_args.model.lr})
 
     # Create the trainer object using our custom logger and set the remaining arguments from the`
     # TrainerConfig.
@@ -56,7 +70,7 @@ if __name__ == "__main__":
         experiment_name=args.expt_name,
         params=args.as_dict(),
         tracking_uri=args.env.mlflow_tracking_uri,
-        meta_fields=getattr(parser, "metafields", {})
+        meta_fields=getattr(parser, "metafields", {}),
     )
     if len(search_results) > 0:
         print("A run with the same hyperparameters already exists. Skipping training.")
