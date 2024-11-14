90a506fcea110b670494b9491223240b50680876
wrongu <richard.d.lange.13@dartmouth.edu>
2024-11-14 12:34:23 -0500

    WIP smarter memory management in NTK

--- src/nn_lib/analysis/ntk.py
+++ src/nn_lib/analysis/ntk.py
@@ -1,11 +1,15 @@
+import logging
+from typing import Callable, Literal, Optional, assert_never
+
+import numpy as np
 import torch
 from torch import nn
 from torch.func import functional_call, vmap, vjp, jvp, jacrev, grad
-from typing import Callable, Literal, Optional, assert_never
 from torch.fx import symbolic_trace
 
 
-# TODO - replace memory exceptions with smart handling of chunk_size in vmap
+GB_PER_ITEM = 4 / 1e9
+
 
 
 def _create_functional_model_as_fn_of_params(model: nn.Module):
@@ -43,6 +47,7 @@ def ntk_in_memory(
     batch_x1: torch.Tensor,
     batch_x2: Optional[torch.Tensor] = None,
     mode: Literal["full", "trace", "diagonal"] = "full",
+    mem_ceiling_gb: float = 10.0,
 ) -> torch.Tensor:
     """Calculate the M1xM2xOxO NTK matrix for a model on a given batch of M1 inputs x1 and M2
     inputs x2, where O is the number of outputs of the model. This is the Jacobian of the model's
@@ -58,12 +63,24 @@ def ntk_in_memory(
     num_params = sum(p.numel() for p in weights.values())
     num_outputs = model_fn(weights, batch_x1[0]).numel()
     numel_jacobian = num_params * num_outputs
-    memory_gb_per_item = numel_jacobian * 4 / 1e9
-    if memory_gb_per_item > 0.5:
+    memory_gb_per_item = numel_jacobian * GB_PER_ITEM
+    if memory_gb_per_item > mem_ceiling_gb:
         raise RuntimeError(
             "Jacobian calculation will use too much memory:"
             f"{num_params} x {num_outputs} elements in the Jacobian, "
-            f"using {memory_gb_per_item:.2f} GB of memory per item in the batch!"
+            f"using {memory_gb_per_item:.2f} GB of memory per item in the batch! "
+            "Consider calling one of the ntk_vjp_* functions instead."
+        )
+    m1 = len(batch_x1)
+    m2 = len(batch_x2) if batch_x2 is not None else m1
+    memory_gb_total = m1 * m2 * num_outputs * num_outputs * GB_PER_ITEM
+    if memory_gb_total > mem_ceiling_gb:
+        raise RuntimeError(
+            "NTK calculation will use too much memory:"
+            f"{m1} x {m2} x {num_outputs} x {num_outputs} elements in the NTK, "
+            f"using {memory_gb_total:.2f} GB of memory! "
+            "Consider calling one of the ntk_vjp_* functions instead "
+            "along with 'trace' or 'diagonal' reduction."
         )
 
     # jac_fn_single_x is a function which takes in a single input and returns the Jacobian of the
@@ -98,35 +115,38 @@ def ntk_task(
     batch_y1: torch.Tensor,
     batch_x2: Optional[torch.Tensor] = None,
     batch_y2: Optional[torch.Tensor] = None,
+    mem_ceiling_gb: float = 10.0,
 ) -> torch.Tensor:
     """Calculate the MxM NTK matrix for a model on a given batch of M inputs using dLoss/dParams
     for each Jacobian.
     """
     model_fn, weights = _create_functional_loss_as_fn_of_params(model, loss_fn)
 
+    num_params = sum(p.numel() for p in weights.values())
+    mem_ceiling_items = mem_ceiling_gb / GB_PER_ITEM
+    auto_chunk_size = int(np.sqrt(mem_ceiling_items / num_params))
+    if auto_chunk_size < len(batch_x1):
+        logging.warning(f"Using chunk size of {auto_chunk_size} for NTK calculation.")
+
     # jac_fn_single_x is a function which takes in a single input and returns the Jacobian of the
     # loss's (L) with respect to the model's weights (w) at that input.
-    jac_fn_single_xy = jacrev(model_fn, argnums=0)
+    grad_fn_single_xy = grad(model_fn, argnums=0)
 
-    # vmap over the batch dimension to get the Jacobian of the model's outputs with respect to
-    # each input in the batch.
-    jac_per_xy1 = vmap(jac_fn_single_xy, (None, 0, 0))(weights, batch_x1, batch_y1)
-    jac_per_xy1 = jac_per_xy1.values()
+    def ntk_task_single_xy(x1, y1, x2, y2):
+        grad1 = grad_fn_single_xy(weights, x1, y1)
+        grad2 = grad_fn_single_xy(weights, x2, y2)
+        return sum(torch.sum(g1 * g2) for g1, g2 in zip(grad1.values(), grad2.values()))
 
     if batch_x2 is not None or batch_y2 is not None:
         if (batch_x2 is None) or (batch_y2 is None):
             raise ValueError("batch_x2 and batch_y2 must be provided together.")
-        jac_per_xy2 = vmap(jac_fn_single_xy, (None, 0, 0))(weights, batch_x2, batch_y2)
-        jac_per_xy2 = jac_per_xy2.values()
     else:
-        jac_per_xy2 = jac_per_xy1
+        batch_x2 = batch_x1
+        batch_y2 = batch_y1
 
-    # Compute the Gram matrix once per parameter group, then sum across all parameters
-    grams = torch.stack(
-        [torch.einsum("N...,M...->NM", jac1, jac2) for jac1, jac2 in zip(jac_per_xy1, jac_per_xy2)],
-        dim=0,
-    )
-    return grams.sum(0)
+    ntk_wrt_x2y2 = vmap(ntk_task_single_xy, (None, None, 0, 0), chunk_size=auto_chunk_size)
+    ntk_wrt_x1y1_x2y2 = vmap(ntk_wrt_x2y2, (0, 0, None, None), chunk_size=auto_chunk_size)
+    return ntk_wrt_x1y1_x2y2(batch_x1, batch_y1, batch_x2, batch_y2)
 
 
 def ntk_vjp(
@@ -134,14 +154,15 @@ def ntk_vjp(
     batch_x1: torch.Tensor,
     batch_x2: Optional[torch.Tensor] = None,
     mode: Literal["full", "trace", "diagonal"] = "full",
+    mem_ceiling_gb: float = 10.0,
 ) -> torch.Tensor:
     match mode:
         case "full":
-            return ntk_vjp_full(model, batch_x1, batch_x2)
+            return ntk_vjp_full(model, batch_x1, batch_x2, mem_ceiling_gb=mem_ceiling_gb)
         case "trace":
-            return ntk_vjp_trace(model, batch_x1, batch_x2)
+            return ntk_vjp_trace(model, batch_x1, batch_x2, mem_ceiling_gb=mem_ceiling_gb)
         case "diagonal":
-            return ntk_vjp_diagonal(model, batch_x1, batch_x2)
+            return ntk_vjp_diagonal(model, batch_x1, batch_x2, mem_ceiling_gb=mem_ceiling_gb)
         case _:
             assert_never(mode)
 
@@ -150,6 +171,7 @@ def ntk_vjp_full(
     model: nn.Module,
     batch_x1: torch.Tensor,
     batch_x2: Optional[torch.Tensor] = None,
+    mem_ceiling_gb: float = 10.0,
 ) -> torch.Tensor:
     """Calculate the M1xM2 NTK matrix for a model on a given batch of M inputs. Equivalent to
     calling ntk_in_memory to get the M1xM2xOxO matrix, then summing over the output dimensions,
@@ -167,22 +189,43 @@ def ntk_vjp_full(
     https://pytorch.org/tutorials/intermediate/neural_tangent_kernels.html
     """
     model_fn, weights = _create_functional_model_as_fn_of_params(model)
+
+    # Memory management heuristic: we'll divide available memory into thirds (multiplicatively).
+    # One third for inner-loop, one third for m1, and one third for m2.
+    mem_ceiling_items = mem_ceiling_gb / GB_PER_ITEM
+    mem_ceiling_items_partitioned = mem_ceiling_items ** (1 / 3)
+    n_params = sum(p.numel() for p in weights.values())
     n_output = model_fn(weights, batch_x1[0]).numel()
+
     m1 = len(batch_x1)
     m2 = len(batch_x2) if batch_x2 is not None else m1
     numel_result = m1 * m2 * n_output * n_output
-    memory_gb = numel_result * 4 / 1e9
-    if memory_gb > 10.0:
+    if numel_result > mem_ceiling_items:
         raise RuntimeError(
             "NTK calculation will use too much memory:"
-            f"{m1} x {m2} x {n_output} x {n_output} elements in the NTK, "
-            f"using {memory_gb:.2f} GB of memory!"
+            f"{m1} x {m2} x {n_output} x {n_output} elements in the full NTK, "
+            f"using {numel_result * GB_PER_ITEM:.2f} GB of memory! "
+            "Consider using ntk_vjp(..., mode='diagonal') or ntk_vjp(..., mode='trace') or "
+            "ntk_task(...) instead."
         )
 
-    basis = torch.eye(n_output, dtype=batch_x1.dtype, device=batch_x1.device).view(n_output, -1)
+    # Inner-loop chunking over the output dimensions
+    numel_inner_ops = 2 * n_params * n_output
+    if numel_inner_ops > mem_ceiling_items_partitioned:
+        # Apply chunking in the innermost loop over the output dimensions.
+        auto_chunk_inner = int(mem_ceiling_items_partitioned / (2 * n_params))
+        numel_inner_ops = 2 * n_params * auto_chunk_inner
+        logging.warning(f"Using inner chunk size of {auto_chunk_inner} for NTK-full calculation.")
+    else:
+        auto_chunk_inner = None
 
-    # TODO - rather than _reduce_ntk at the end, we could be a lot more efficient by modifying
-    #  the vjp and jvp behavior to do the reduction as we go. Can also reduce memory usage.
+    auto_chunk_outer = int((mem_ceiling_items / numel_inner_ops) ** (1 / 2))
+    if auto_chunk_outer < len(batch_x1):
+        logging.warning(f"Using chunk size of {auto_chunk_outer} for NTK calculation.")
+    else:
+        auto_chunk_outer = None
+
+    basis = torch.eye(n_output, dtype=batch_x1.dtype, device=batch_x1.device).view(n_output, -1)
 
     def get_oxo_ntk(x1, x2):
         # We've already wrapped the model in a function of the parameters, but we need to wrap
@@ -198,13 +241,16 @@ def ntk_vjp_full(
             _, jvps = jvp(lambda w: model_fn(w, x2), (weights,), j2_times_vec)
             return jvps
 
-        return vmap(get_ox1_ntk_slice, chunk_size=100)(basis)
+        return vmap(get_ox1_ntk_slice, chunk_size=auto_chunk_inner)(basis)
 
     # ``get_oxo_ntk(x1, x2)`` computes the OxO NTK for a single pair (x1, x2). Now we vmap over
     # the batches.
     if batch_x2 is None:
         batch_x2 = batch_x1
-    result = vmap(vmap(get_oxo_ntk, (None, 0)), (0, None))(batch_x1, batch_x2)
+
+    ntk_wrt_x2 = vmap(get_oxo_ntk, (None, 0), chunk_size=auto_chunk_outer)
+    ntk_wrt_x1x2 = vmap(ntk_wrt_x2, (0, None), chunk_size=auto_chunk_outer)
+    result = ntk_wrt_x1x2(batch_x1, batch_x2)
 
     return result
 
@@ -213,24 +259,45 @@ def ntk_vjp_diagonal(
     model: nn.Module,
     batch_x1: torch.Tensor,
     batch_x2: Optional[torch.Tensor] = None,
+    mem_ceiling_gb: float = 10.0,
 ) -> torch.Tensor:
     """See ntk_vjp_full; this is an optimized version for mode="trace"."""
     model_fn, weights = _create_functional_model_as_fn_of_params(model)
+
+    mem_ceiling_items = mem_ceiling_gb / GB_PER_ITEM
+    mem_ceiling_items_partitioned = mem_ceiling_items ** (1 / 3)
     n_output = model_fn(weights, batch_x1[0]).numel()
+    n_params = sum(p.numel() for p in weights.values())
+
     m1 = len(batch_x1)
     m2 = len(batch_x2) if batch_x2 is not None else m1
-    numel_intermediate = m1 * m2 * n_output
-    memory_gb = numel_intermediate * 4 / 1e9
-    if memory_gb > 10.0:
+    numel_result = m1 * m2 * n_output
+    if numel_result > mem_ceiling_items:
         raise RuntimeError(
             "NTK calculation will use too much memory:"
-            f"{m1} x {m2} x {n_output} elements in the NTK, "
-            f"using {memory_gb:.2f} GB of memory!"
+            f"{m1} x {m2} x {n_output} elements in the diagonal NTK, "
+            f"using {numel_result * GB_PER_ITEM:.2f} GB of memory! "
+            "Consider using ntk_vjp(..., mode='trace') or ntk_task(...) instead."
         )
+
+    numel_inner_ops = 2 * n_params * n_output
+    if numel_inner_ops > mem_ceiling_items_partitioned:
+        # Apply chunking in the innermost loop over the output dimensions.
+        auto_chunk_inner = int(mem_ceiling_items_partitioned / (2 * n_params))
+        numel_inner_ops = 2 * n_params * auto_chunk_inner
+        logging.warning(f"Using inner chunk size of {auto_chunk_inner} for NTK-trace calculation.")
+    else:
+        auto_chunk_inner = None
+
+    auto_chunk_outer = int((mem_ceiling_items / numel_inner_ops) ** (1 / 2))
+    if auto_chunk_outer < len(batch_x1):
+        logging.warning(f"Using chunk size of {auto_chunk_outer} for NTK calculation.")
+    else:
+        auto_chunk_outer = None
 
     basis = torch.eye(n_output, dtype=batch_x1.dtype, device=batch_x1.device).view(n_output, -1)
 
-    def get_trace_ntk(x1, x2):
+    def get_diagonal_ntk(x1, x2):
         def get_ntk_vv(vec):
             # Isolate the input -> output[j] function and get grads wrt params for x1 and x2
             jac_fn1 = grad(lambda w: torch.sum(model_fn(w, x1) * vec), argnums=0)
@@ -242,11 +309,14 @@ def ntk_vjp_diagonal(
                 for jac1, jac2 in zip(jac_fn1(weights).values(), jac_fn2(weights).values())
             )
 
-        return vmap(get_ntk_vv, chunk_size=100)(basis)
+        return vmap(get_ntk_vv, chunk_size=auto_chunk_inner)(basis)
 
     if batch_x2 is None:
         batch_x2 = batch_x1
-    result = vmap(vmap(get_trace_ntk, (None, 0)), (0, None))(batch_x1, batch_x2)
+
+    ntk_wrt_x2 = vmap(get_diagonal_ntk, (None, 0), chunk_size=auto_chunk_outer)
+    ntk_wrt_x1x2 = vmap(ntk_wrt_x2, (0, None), chunk_size=auto_chunk_outer)
+    result = ntk_wrt_x1x2(batch_x1, batch_x2)
 
     return result
 
@@ -255,40 +325,64 @@ def ntk_vjp_trace(
     model: nn.Module,
     batch_x1: torch.Tensor,
     batch_x2: Optional[torch.Tensor] = None,
+    mem_ceiling_gb: float = 10.0,
 ) -> torch.Tensor:
     """See ntk_vjp_full; this is an optimized version for mode="trace"."""
     model_fn, weights = _create_functional_model_as_fn_of_params(model)
+
+    mem_ceiling_items = mem_ceiling_gb / GB_PER_ITEM
+    mem_ceiling_items_partitioned = mem_ceiling_items ** (1 / 3)
     n_output = model_fn(weights, batch_x1[0]).numel()
+    n_params = sum(p.numel() for p in weights.values())
+
     m1 = len(batch_x1)
     m2 = len(batch_x2) if batch_x2 is not None else m1
-    numel_intermediate = m1 * m2 * n_output
-    memory_gb = numel_intermediate * 4 / 1e9
-    if memory_gb > 10.0:
+    numel_result = m1 * m2
+    if numel_result > mem_ceiling_items:
         raise RuntimeError(
             "NTK calculation will use too much memory:"
-            f"{m1} x {m2} x {n_output} elements in the NTK, "
-            f"using {memory_gb:.2f} GB of memory!"
+            f"{m1} x {m2} elements in the diagonal NTK, "
+            f"using {numel_result * GB_PER_ITEM:.2f} GB of memory! "
+            "Consider using smaller batches."
         )
+
+    numel_inner_ops = 2 * n_params * n_output
+    if numel_inner_ops > mem_ceiling_items_partitioned:
+        # Apply chunking in the innermost loop over the output dimensions.
+        auto_chunk_inner = int(mem_ceiling_items_partitioned / (2 * n_params))
+        numel_inner_ops = 2 * n_params * auto_chunk_inner
+        logging.warning(f"Using inner chunk size of {auto_chunk_inner} for NTK-trace calculation.")
+    else:
+        auto_chunk_inner = None
+
+    auto_chunk_outer = int((mem_ceiling_items / numel_inner_ops) ** (1 / 2))
+    if auto_chunk_outer < len(batch_x1):
+        logging.warning(f"Using chunk size of {auto_chunk_outer} for NTK calculation.")
+    else:
+        auto_chunk_outer = None
 
     basis = torch.eye(n_output, dtype=batch_x1.dtype, device=batch_x1.device).view(n_output, -1)
 
     def get_trace_ntk(x1, x2):
         def get_ntk_vv(vec):
             # Isolate the input -> output[j] function and get grads wrt params for x1 and x2
-            jac_fn1 = grad(lambda w: torch.sum(model_fn(w, x1) * vec), argnums=0)
-            jac_fn2 = grad(lambda w: torch.sum(model_fn(w, x2) * vec), argnums=0)
+            grad_fn1 = grad(lambda w: torch.sum(model_fn(w, x1) * vec), argnums=0)
+            grad_fn2 = grad(lambda w: torch.sum(model_fn(w, x2) * vec), argnums=0)
 
             # Compute the (scalar) value for Gram(x1, x2)_{jj}
             return sum(
-                torch.sum(jac1 * jac2)
-                for jac1, jac2 in zip(jac_fn1(weights).values(), jac_fn2(weights).values())
+                torch.sum(grad1 * grad2)
+                for grad1, grad2 in zip(grad_fn1(weights).values(), grad_fn2(weights).values())
             )
 
-        return torch.sum(vmap(get_ntk_vv, chunk_size=100)(basis))
+        return torch.sum(vmap(get_ntk_vv, chunk_size=auto_chunk_inner)(basis))
 
     if batch_x2 is None:
         batch_x2 = batch_x1
-    result = vmap(vmap(get_trace_ntk, (None, 0)), (0, None))(batch_x1, batch_x2)
+
+    ntk_wrt_x2 = vmap(get_trace_ntk, (None, 0), chunk_size=auto_chunk_outer)
+    ntk_wrt_x1x2 = vmap(ntk_wrt_x2, (0, None), chunk_size=auto_chunk_outer)
+    result = ntk_wrt_x1x2(batch_x1, batch_x2)
 
     return result
 
@@ -299,25 +393,45 @@ if __name__ == "__main__":
     from nn_lib.datasets import ImageNetDataModule, get_tv_default_transforms
 
     model = symbolic_trace(get_pretrained_model("resnet18")).eval()
-    data = ImageNetDataModule(root_dir="/data/datasets/", batch_size=2)
+    data = ImageNetDataModule(root_dir="/data/datasets/", batch_size=7)
     data.test_transform = get_tv_default_transforms("resnet18")
     data.prepare_data()
     data.setup("test")
     dl = data.test_dataloader()
 
     x, y = next(iter(dl))
+    device = "cuda:1"
 
     start = time.time()
-    g = ntk_task(model.to("cuda:0"), nn.CrossEntropyLoss(), x.to("cuda:0"), y.to("cuda:0"))
+    g = ntk_task(
+        model.to(device),
+        nn.CrossEntropyLoss(),
+        x.to(device),
+        y.to(device),
+        mem_ceiling_gb=20.0,
+    )
     print("Time taken (ntk_task):", time.time() - start)
     print("NTK matrix shape:", g.shape)
 
     start = time.time()
-    g = ntk_vjp_trace(model.to("cuda:0"), x.to("cuda:0"))
+    g = ntk_vjp_trace(
+        model.to(device),
+        x.to(device),
+        mem_ceiling_gb=20.0,
+    )
     print("Time taken (ntk_vjp_trace):", time.time() - start)
     print("NTK matrix shape:", g.shape)
 
     start = time.time()
-    g = ntk_vjp_full(model.to("cuda:0"), x.to("cuda:0"))
-    print("Time taken (ntk_vjp_full):", time.time() - start)
+    g = ntk_vjp_diagonal(
+        model.to(device),
+        x.to(device),
+        mem_ceiling_gb=20.0,
+    )
+    print("Time taken (ntk_vjp_diagonal):", time.time() - start)
     print("NTK matrix shape:", g.shape)
+
+    # start = time.time()
+    # g = ntk_vjp_full(model.to(device), x.to(device))
+    # print("Time taken (ntk_vjp_full):", time.time() - start)
+    # print("NTK matrix shape:", g.shape)
